9ddec75d07d48d4559e8e9c52526bb32106772ab
networkisvn-cell <networkisvn@gmail.com>
2026-01-22 00:18:48 +0600

    Add Navbar component with Discord link

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "20px",
+        display: "flex",
+        justifyContent: "space-between",
+        background: "#0b0f1a"
+      }}
+    >
+      <h2>💀 Skeleton Army</h2>
+      <a href="https://discord.gg/YOUR_INVITE" target="_blank">
+        Join Discord
+      </a>
+    </nav>
+  );
+}
