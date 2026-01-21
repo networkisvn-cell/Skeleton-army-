de99d0c59fdc3b1aca1454c71a34d531faaea6ba
networkisvn-cell <networkisvn@gmail.com>
2026-01-21 23:48:58 +0600

    Add Footer component with styling and copyright

---
+++ pagespages/components/components/components/components/components/lib/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      padding: "20px",
+      textAlign: "center",
+      background: "#0b0f1a"
+    }}>
+      © 2026 Skeleton Army | Powered by Next.js
+    </footer>
+  );
+}
